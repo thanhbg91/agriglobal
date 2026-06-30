a3be41c40ae93f6fa000f95878ab6afc9e364d89
thanhbg91 <134175660+thanhbg91@users.noreply.github.com>
2026-07-01 00:10:32 +0700

    Update pi-sdk.js

--- pi-sdk.js
+++ pi-sdk.js
@@ -1,1 +1,45 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.Pi=t():e.Pi=t()}(window,(function(){return function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}return n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:t})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";n.r(t);var o=function(){function e(){this.version=null,this.sandbox=null,this.onIncompletePaymentFound=null}return e.prototype.init=function(e){if(!e.version)throw new Error("version is required");if("2.0"!==e.version)throw new Error("version must be 2.0");this.version=e.version,this.sandbox=e.sandbox||!1},e.prototype.authenticate=function(e,t){var n=this;if(!this.version)throw new Error("Pi.init must be called before Pi.authenticate");if(!e||0===e.length)throw new Error("scopes are required");if(!t)throw new Error("onIncompletePaymentFound is required");this.onIncompletePaymentFound=t;var o=window.parent;return new Promise((function(t,r){var i=function(e){if(e.origin.window===o&&e.data&&"pi_auth_result"===e.data.type){window.removeEventListener("message",i);var n=e.data.payload;n.error?r(n.error):t(n)}};window.addEventListener("message",i),o.postMessage({type:"pi_auth_request",payload:{scopes:e,sandbox:n.sandbox}},"*")}))},e.prototype.createPayment=function(e,t){var n=this;if(!this.onIncompletePaymentFound)throw new Error("Pi.authenticate must be called before Pi.createPayment");var o=window.parent;return new Promise((function(r,i){var a=function(e){if(e.origin.window===o&&e.data&&"pi_payment_result"===e.data.type){window.removeEventListener("message",a);var n=e.data.payload;n.error?i(n.error):r(n)}};window.addEventListener("message",a),o.postMessage({type:"pi_payment_request",payload:{payment:e,callbacks:t,sandbox:n.sandbox}},"*")}))},e}();t.default=new o,window.Pi=t.default}])}));
+if (!window.Pi) {
+    var Pi = {
+        _onIncompletePaymentFound: null,
+        init: function(e) {
+            if (!e.version) throw new Error("version is required");
+            if ("2.0" !== e.version) throw new Error("Unsupported version");
+            this.version = e.version;
+            this.sandbox = e.sandbox || false;
+        },
+        authenticate: function(e, t) {
+            if (!this.version) throw new Error("Pi.init must be called before Pi.authenticate");
+            if (!e || 0 === e.length) throw new Error("scopes are required");
+            if (!t) throw new Error("onIncompletePaymentFound is required");
+            this._onIncompletePaymentFound = t;
+            var n = window.parent;
+            return new Promise(function(t, o) {
+                var r = function(e) {
+                    if (e.origin.window === n && e.data && "pi_auth_result" === e.data.type) {
+                        window.removeEventListener("message", r);
+                        var i = e.data.payload;
+                        i.error ? o(i.error) : t(i);
+                    }
+                };
+                window.addEventListener("message", r);
+                n.postMessage({ type: "pi_auth_request", payload: { scopes: e, sandbox: Pi.sandbox } }, "*");
+            });
+        },
+        createPayment: function(e, t) {
+            if (!this._onIncompletePaymentFound) throw new Error("Pi.authenticate must be called before Pi.createPayment");
+            var n = window.parent;
+            return new Promise(function(o, r) {
+                var s = function(e) {
+                    if (e.origin.window === n && e.data && "pi_payment_result" === e.data.type) {
+                        window.removeEventListener("message", s);
+                        var i = e.data.payload;
+                        i.error ? r(i.error) : o(i);
+                    }
+                };
+                window.addEventListener("message", s);
+                n.postMessage({ type: "pi_payment_request", payload: { payment: e, callbacks: t, sandbox: Pi.sandbox } }, "*");
+            });
+        }
+    };
+    window.Pi = Pi;
+}
